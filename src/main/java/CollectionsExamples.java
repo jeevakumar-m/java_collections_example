4a834954714be0f8fda4552715218f6cfacfc2fa
jeeva <jeevakumar.moorthy@gmail.com>
2023-12-11 23:45:49 +0530

    added list_arrayList()

--- src/main/java/CollectionsExamples.java
+++ src/main/java/CollectionsExamples.java
@@ -15,8 +15,53 @@ public class CollectionsExamples {
         fruits.add(5,"papaya");
         System.out.println(fruits);
 
+        // find whether Fruit1
+        System.out.println("is fruit1 present -- " + fruits.contains("Fruit1"));
+        // to access speicifi index element
+        System.out.println("4th index element -- " + fruits.get(4));
+        // to access first and Last element
+        System.out.println("1st Elment : " + fruits.getFirst());
+        System.out.println("lasst Elment : " + fruits.getLast());
 
+        fruits.removeFirst();
+        fruits.add(0,"Anton");
 
+        System.out.println(fruits);
+
+        Iterator<String> it = fruits.iterator();
+
+        while(it.hasNext())
+        {
+            String s= it.next();
+            System.out.println("IT Elements : " +  s);
+            if (s.equals("Anton"))
+            {
+                it.remove();
+            }
+        }
+
+        System.out.println(fruits);
+
+        // 1,2,3, ....,7600,.... 10000
+        ListIterator<String> lit = fruits.listIterator();
+
+        while(lit.hasNext())
+        {
+            System.out.println("LIT Elements : " +  lit.next());
+        }
+
+        while(lit.hasPrevious())
+        {
+            String s = lit.previous();
+            System.out.println("LIT PREV Elements : " +  s);
+            if (s.equals("Cucumber"))
+            {
+                lit.remove();
+                lit.add("KJeevasd");
+            }
+        }
+
+        System.out.println(fruits);
 
     }
 
