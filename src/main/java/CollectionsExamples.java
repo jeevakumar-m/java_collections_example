34dc179c0b3acb223aed5f22cc872ab2ae03571a
jeeva <jeevakumar.moorthy@gmail.com>
2023-12-12 00:54:36 +0530

    added stack_balancer()

--- src/main/java/CollectionsExamples.java
+++ src/main/java/CollectionsExamples.java
@@ -150,6 +150,129 @@ public class CollectionsExamples {
         System.out.println(fruits);
     }
 
+    public static void list_stack()
+    {
+        String[] fruitArray = {"Fruit1", "Fruit2"};
+        List<String> fruits = new Stack<String>();
+        fruits.add("Bananan"); // 0th
+        fruits.add("Apple");  // 1st
+        fruits.add("Cucumber"); //2nd
+        fruits.addAll(Arrays.asList(fruitArray));
+        System.out.println(fruits);
+        fruits.add(5,"papaya");
+        System.out.println(fruits);
+
+        // find whether Fruit1
+        System.out.println("is fruit1 present -- " + fruits.contains("Fruit1"));
+        // to access speicifi index element
+        System.out.println("4th index element -- " + fruits.get(4));
+        // to access first and Last element
+        System.out.println("1st Elment : " + fruits.getFirst());
+        System.out.println("lasst Elment : " + fruits.getLast());
+
+        fruits.removeFirst();
+        fruits.add(0,"Anton");
+
+
+        System.out.println(fruits);
+        System.out.println(fruits.get(0));
+
+        for (String s: fruits)
+        {
+            System.out.println("modern for loop=" + s);
+        }
+
+        for(int i=0;i<fruits.size();i++)
+        {
+            System.out.println("Classical for =" + fruits.get(i));
+        }
+
+        Iterator<String> it = fruits.iterator();
+
+        while(it.hasNext())
+        {
+            String s= it.next();
+            System.out.println("IT Elements : " +  s);
+            if (s.equals("Anton"))
+            {
+                it.remove();
+            }
+        }
+
+        System.out.println(fruits);
+
+        // 1,2,3, ....,7600,.... 10000
+        ListIterator<String> lit = fruits.listIterator();
+
+        while(lit.hasNext())
+        {
+            System.out.println("LIT Elements : " +  lit.next());
+        }
+
+        while(lit.hasPrevious())
+        {
+            String s = lit.previous();
+            System.out.println("LIT PREV Elements : " +  s);
+            if (s.equals("Cucumber"))
+            {
+                lit.remove();
+                lit.add("KJeevasd");
+            }
+        }
+
+        System.out.println(fruits);
+    }
+
+    public static void stack_example(String expression)
+    {
+
+        boolean isValid = true;
+        Stack<Character> exp_balancer = new Stack<>();
+        Character comp = null;
+        for(Character ch : expression.toCharArray())
+        {
+            if (ch=='{' || ch == '(' || ch == '[')
+            {
+                //push
+                exp_balancer.push(ch);
+            }
+            else {
+
+
+                    switch (ch) {
+                        case '}':
+                            if(!exp_balancer.isEmpty())
+                            {comp = exp_balancer.pop();}
+                            if (comp != '{' ) {
+                                isValid = false;
+                            }
+                            break;
+                        case ']':
+                            if(!exp_balancer.isEmpty())
+                            {comp = exp_balancer.pop();}
+                            if (comp != '[' ) {
+                                isValid = false;
+                            }
+                            break;
+                        case ')':
+                            if(!exp_balancer.isEmpty())
+                            {comp = exp_balancer.pop();}
+                            if ( comp != '(') {
+                                isValid = false;
+                            }
+                            break;
+                    }
+
+            }
+
+            if(!isValid)
+            {
+              break;
+            }
+        }
+
+        System.out.println("Is this a valid expression : " + isValid);
+    }
 
 
 
@@ -161,7 +284,13 @@ public class CollectionsExamples {
 
     public static void main(String args[])
     {
-        list_linkedList();
+        stack_example("({[{}()]})");
+        stack_example("{{[[(())]]}}");
+        stack_example("{}[]()");
+        stack_example("({[{}()]})}");
+        stack_example("{{[[(())]}}");
+
+
     }
 }
 
