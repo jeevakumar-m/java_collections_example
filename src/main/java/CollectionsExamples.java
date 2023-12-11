f839e564ea6d18e38eb23fbd60870e8979814ffc
jeeva <jeevakumar.moorthy@gmail.com>
2023-12-12 00:09:03 +0530

    added list_linkedList()

--- src/main/java/CollectionsExamples.java
+++ src/main/java/CollectionsExamples.java
@@ -77,7 +77,78 @@ public class CollectionsExamples {
 
     }
 
-    public static void list
+    public static void list_linkedList()
+    {
+        String[] fruitArray = {"Fruit1", "Fruit2"};
+        List<String> fruits = new LinkedList<String>();
+        fruits.add("Bananan"); // 0th
+        fruits.add("Apple");  // 1st
+        fruits.add("Cucumber"); //2nd
+        fruits.addAll(Arrays.asList(fruitArray));
+        System.out.println(fruits);
+        fruits.add(5,"papaya");
+        System.out.println(fruits);
+
+        // find whether Fruit1
+        System.out.println("is fruit1 present -- " + fruits.contains("Fruit1"));
+        // to access speicifi index element
+        System.out.println("4th index element -- " + fruits.get(4));
+        // to access first and Last element
+        System.out.println("1st Elment : " + fruits.getFirst());
+        System.out.println("lasst Elment : " + fruits.getLast());
+
+        fruits.removeFirst();
+        fruits.add(0,"Anton");
+
+
+        System.out.println(fruits);
+        System.out.println(fruits.get(0));
+
+        for (String s: fruits)
+        {
+            System.out.println("modern for loop=" + s);
+        }
+
+        for(int i=0;i<fruits.size();i++)
+        {
+            System.out.println("Classical for =" + fruits.get(i));
+        }
+
+        Iterator<String> it = fruits.iterator();
+
+        while(it.hasNext())
+        {
+            String s= it.next();
+            System.out.println("IT Elements : " +  s);
+            if (s.equals("Anton"))
+            {
+                it.remove();
+            }
+        }
+
+        System.out.println(fruits);
+
+        // 1,2,3, ....,7600,.... 10000
+        ListIterator<String> lit = fruits.listIterator();
+
+        while(lit.hasNext())
+        {
+            System.out.println("LIT Elements : " +  lit.next());
+        }
+
+        while(lit.hasPrevious())
+        {
+            String s = lit.previous();
+            System.out.println("LIT PREV Elements : " +  s);
+            if (s.equals("Cucumber"))
+            {
+                lit.remove();
+                lit.add("KJeevasd");
+            }
+        }
+
+        System.out.println(fruits);
+    }
 
 
 
@@ -90,7 +161,7 @@ public class CollectionsExamples {
 
     public static void main(String args[])
     {
-        list_arrayList();
+        list_linkedList();
     }
 }
 
