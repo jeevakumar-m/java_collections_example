6cd068ab976f98056d3c58f1729765b2bd72dd98
jeeva <jeevakumar.moorthy@gmail.com>
2023-12-11 23:20:39 +0530

    added list_arrayList()

--- src/main/java/CollectionsExamples.java
+++ src/main/java/CollectionsExamples.java
@@ -1,6 +1,40 @@
+import java.util.*;
+
 public class CollectionsExamples {
+
+    public static void list_arrayList()
+    {
+        String[] fruitArray = {"Fruit1", "Fruit2"};
+        List<String> fruits = new ArrayList<String>();
+
+        fruits.add("Bananan"); // 0th
+        fruits.add("Apple");  // 1st
+        fruits.add("Cucumber"); //2nd
+        fruits.addAll(Arrays.asList(fruitArray));
+        System.out.println(fruits);
+        fruits.add(5,"papaya");
+        System.out.println(fruits);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
     public static void main(String args[])
     {
-        System.out.println("Hello");
+        list_arrayList();
     }
 }
+
+
+
+
